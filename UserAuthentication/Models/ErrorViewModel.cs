a80abb87d23645e94b169af55ce520937562e303
mdshahzebalam <sus841226@gmail.com>
2025-01-16 20:46:16 +0530

    Add project files.

---
+++ UserAuthentication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UserAuthentication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
